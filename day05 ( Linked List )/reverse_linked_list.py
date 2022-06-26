3d719c9b3a76b088635669c32472635213e8d2af
Eugene Gwee <eugene.gzj@circles.asia>
2022-06-26 12:21:48 +0800

    feat: create Reverse Linked List python solution

---
+++ day05 ( Linked List )/reverse_linked_list.py
@@ -0,0 +1,28 @@
+# Date Completed: 26th June 2022
+# Resources:
+# 1. https://www.youtube.com/watch?v=iRtLEoL-r-g&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=28
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        oldNode = None
+
+        # loop
+        while head is not None:
+            nextNode = head.next  # 2,3,4,5,None
+            head.next = oldNode  # None,1,2,3,4
+            oldNode = head  # 1,2,3,4,5
+            head = nextNode  # 2,3,4,5,None
+
+        return oldNode
+
+
+# Success
+# Details
+# Runtime: 51 ms, faster than 57.61% of Python3 online submissions for Reverse Linked List.
+# Memory Usage: 15.4 MB, less than 55.36% of Python3 online submissions for Reverse Linked List.
